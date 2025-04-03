97d70e52a0f5e15c01fcff7dec8b8068137c29b8
Edurne Pascual <herenvardo@gmail.com>
2025-04-03 11:40:07 +0200

    chore: update Stats stories

--- src/stories/Creature/Stats/DeferredMultiColumnStatsView.stories.tsx
+++ src/stories/Creature/Stats/DeferredMultiColumnStatsView.stories.tsx
@@ -3,6 +3,7 @@ import type { MultiStats, ViewProps } from '../../..';
 import { MultiColumnStatsView } from '../../..';
 import { Deferred } from '../../../hocs/Deferred';
 import { DeferredViewProps } from '../../../helpers/wrappers';
+import { statMocks } from './helpers';
 
 type DeferredStatsProps = Omit<DeferredViewProps<MultiStats>, 'Loaded'>;
 
@@ -15,52 +16,11 @@ export default {
     component: DeferredStats,
 };
 
-const rawData: MultiStats = [
-    {
-        key: 'charsheet.common.base',
-        strength: 10,
-        dexterity: 10,
-        constitution: 10,
-        intelligence: 10,
-        wisdom: 10,
-        charisma: 10,
-    },
-    {
-        key: 'charsheet.common.gear',
-        isMod: true,
-        strength: 2,
-        dexterity: 3,
-        constitution: 4,
-        intelligence: 1,
-        wisdom: 6,
-        charisma: 5,
-    },
-    {
-        key: 'charsheet.common.total',
-        strength: 12,
-        dexterity: 13,
-        constitution: 14,
-        intelligence: 11,
-        wisdom: 16,
-        charisma: 15,
-    },
-    {
-        key: 'charsheet.common.mod',
-        isMod: true,
-        strength: 1,
-        dexterity: 1,
-        constitution: 2,
-        intelligence: 0,
-        wisdom: 3,
-        charisma: 2,
-    },
-];
-
 const delay = 5000;
 
 const dataPromise = new Promise<ViewProps<MultiStats>>(resolve => {
     setTimeout(() => {
-        resolve({ data: rawData });
+        resolve({ data: statMocks.multiStats });
     }, delay);
 });
 
--- src/stories/Creature/Stats/MultiColumnStatsView.stories.tsx
+++ src/stories/Creature/Stats/MultiColumnStatsView.stories.tsx
@@ -1,5 +1,6 @@
 import type { MultiStats, ViewProps } from '../../..';
 import { MultiColumnStatsView } from '../../..';
+import { statMocks } from './helpers';
 
 export default {
     title: 'Creature/Stats/MultiColumnStatsView',
@@ -11,44 +12,5 @@ const Template = ({ data }: ViewProps<MultiStats>): React.ReactNode => (
 );
 
 export const Default = Template.bind(this, {
-    data: [
-        {
-            key: 'charsheet.common.base',
-            strength: 10,
-            dexterity: 10,
-            constitution: 10,
-            intelligence: 10,
-            wisdom: 10,
-            charisma: 10,
-        },
-        {
-            key: 'charsheet.common.gear',
-            isMod: true,
-            strength: 2,
-            dexterity: 3,
-            constitution: 4,
-            intelligence: 1,
-            wisdom: 6,
-            charisma: 5,
-        },
-        {
-            key: 'charsheet.common.total',
-            strength: 12,
-            dexterity: 13,
-            constitution: 14,
-            intelligence: 11,
-            wisdom: 16,
-            charisma: 15,
-        },
-        {
-            key: 'charsheet.common.mod',
-            isMod: true,
-            strength: 1,
-            dexterity: 1,
-            constitution: 2,
-            intelligence: 0,
-            wisdom: 3,
-            charisma: 2,
-        },
-    ],
+    data: statMocks.multiStats,
 });
