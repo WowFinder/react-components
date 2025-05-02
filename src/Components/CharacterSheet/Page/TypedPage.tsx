bbc200a46050998eec9804b50936e0da5e8aa968
Edurne Pascual <herenvardo@gmail.com>
2025-05-03 00:19:38 +0200

    partial: initial structure for Character Sheet pages

---
+++ src/Components/CharacterSheet/Page/TypedPage.tsx
@@ -0,0 +1,55 @@
+import { ReactElement, useState } from 'react';
+import type {
+    CharXpProps,
+    FullPageSelection,
+    PageArgs,
+    PartialPageSelection,
+    TypedPageArgs,
+} from './types';
+import { PageType } from './types';
+
+const pageTypes: { [keys in PageType]: React.FC<PageArgs> } = {
+    /* TODO https://github.com/WowFinder/react-components/issues/148 */ [PageType.main]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/149 */ [PageType.skills]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/150 */ [PageType.inventory]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/151 */ [PageType.magicScores]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/152 */ [PageType.spells]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/153 */ [PageType.feral]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/154 */ [PageType.travel]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/155 */ [PageType.moonkin]:
+        () => <></>,
+    /* TODO https://github.com/WowFinder/react-components/issues/156 */ [PageType.tree]:
+        () => <></>,
+};
+
+const defaultPages: FullPageSelection = {
+    [PageType.main]: true,
+    [PageType.skills]: true,
+    [PageType.inventory]: true,
+    [PageType.magicScores]: true,
+    [PageType.spells]: false,
+    [PageType.feral]: true,
+    [PageType.travel]: false,
+    [PageType.moonkin]: false,
+    [PageType.tree]: false,
+};
+
+function TypedPage<T extends CharXpProps = CharXpProps>({
+    type,
+    ...props
+}: TypedPageArgs<T>): ReactElement {
+    const [selectedPages] = useState<PartialPageSelection>(defaultPages);
+    return pageTypes[type]({
+        ...props,
+        visible: !!selectedPages[type],
+    }) as ReactElement;
+}
+
+export { TypedPage };
---
+++ src/Components/CharacterSheet/Page/types.ts
@@ -0,0 +1,46 @@
+import { CreatureFacade } from '@wowfinder/model';
+
+enum PageType {
+    main = 'main',
+    skills = 'skills',
+    inventory = 'inventory',
+    magicScores = 'magicScores',
+    spells = 'spells',
+    feral = 'feralForms',
+    travel = 'travelForms',
+    moonkin = 'moonkinForm',
+    tree = 'treeForm',
+}
+
+type CharProps = {
+    char?: CreatureFacade;
+};
+
+type CharXpProps = CharProps & {
+    char?: CreatureFacade;
+    xp?: number;
+};
+
+type PageArgs<T extends CharXpProps = CharXpProps> = T & { visible: boolean };
+
+type TypedPageArgs<T extends CharXpProps = CharXpProps> = PageArgs<T> & {
+    type: PageType;
+};
+
+type HitPointProps = Partial<CharProps> & { current?: number };
+
+type PartialPageSelection = { [keys in PageType]?: boolean };
+
+type FullPageSelection = { [keys in PageType]: boolean };
+
+export { PageType };
+
+export type {
+    CharProps,
+    CharXpProps,
+    HitPointProps,
+    PageArgs,
+    TypedPageArgs,
+    PartialPageSelection,
+    FullPageSelection,
+};
